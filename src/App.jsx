0e42d246c9d65ecc095c7932f4f23cb874caacc0
JaffarShah <sjeffshah@gmail.com>
2019-05-07 12:24:58 -0400

    Test push command on git

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 import React, {Component} from 'react';
 
+export default class TimerComponent extends Components {
+
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+}
+
 class App extends Component {
   render() {
     return (
