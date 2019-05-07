d006499aa4fa92bbd456356ef978e59ab90fef14
JaffarShah <sjeffshah@gmail.com>
2019-05-07 17:20:55 -0400

    Create MessageList file, import html from index and export default, fix class to className, Pass messages as props.

---
+++ src/MessageList.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import Message from './Message.jsx';
+
+class MessageList extends Component {
+    render() {
+        let messages = this.props.messages.map((message) =>
+          <Message key = {message.id} username = {message.username} content = {message.content}/>)
+        return (
+            <main className="allmessages">
+            {messages}
+            </main>
+        );
+    }
+}
+export default MessageList;
