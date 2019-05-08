ea4b41bd546d4350c87c020254308d24e9a5cd04
JaffarShah <sjeffshah@gmail.com>
2019-05-08 19:03:46 -0400

    Here, I am committing this part because the mentor did all this and broke the whole code which was working. Now, I have to clean it, modify it and fix it again. What a waste of my whole day.

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const SocketServer = require('ws').Server;
+const WebSocket = require('ws');
 const uuidV1 = require('uuid/v1');
 
 // Set the port to 3001
@@ -12,24 +12,43 @@ const server = express()
   .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
 
 // Create the WebSockets server
-const wss = new SocketServer({ server });
+const wss = new WebSocket.Server({ server });
 
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
-wss.on('connection', (ws) => {
-    console.log('Client connected');
-
-
-ws.on('message', function incoming(newMessage) {
-    const parsedMessage = JSON.parse(newMessage);
-        //console.log(parsedMessage);
-        console.log(`User ${parsedMessage.username} said ${parsedMessage.content}`);
-        parsedMessage.id = uuidV1();
-        //console.log('parsedMessage is', parsedMessage);
-        ws.send(JSON.stringify(parsedMessage));
+wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(data) {
+        console.log(data);
+      // Broadcast to everyone else.
+      wss.clients.forEach(function each(client) {
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
+          client.send(data);
+        }
       });
+    });
+  });
 
-  // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-  ws.on('close', () => console.log('Client disconnected'));
-});
+
+
+
+
+
+
+
+
+//     console.log('Client connected');
+
+
+// ws.on('message', function incoming(newMessage) {
+//     const parsedMessage = JSON.parse(newMessage);
+//         //console.log(parsedMessage);
+//         console.log(`User ${parsedMessage.username} said ${parsedMessage.content}`);
+//         parsedMessage.id = uuidV1();
+//         //console.log('parsedMessage is', parsedMessage);
+//         ws.send(JSON.stringify(parsedMessage));
+//       });
+
+//   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
+//   ws.on('close', () => console.log('Client disconnected'));
+// });
