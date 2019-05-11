07ef3f728860a6a39a20bb9ac5627ce68ccf86f7
JaffarShah <sjeffshah@gmail.com>
2019-05-10 21:08:17 -0400

    Clean code, remove dead codes

--- backend/chatty_server/server.js
+++ backend/chatty_server/server.js
@@ -5,50 +5,49 @@ const uuidV1 = require('uuid/v1'); //This generates a v1 UUID = universally uniq
 // Set the port to 3001
 const PORT = 3001;
 
-// New express server
+// Create a new express server
 const server = express()
   .use(express.static('public'))
   .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
 
-// WebSocket Server
+  // The WebSockets server
 const wss = new SocketServer({ server });
 
-
 wss.on('connection', (ws) => {
-  wss.broadcast = function broadcast(data) {
+wss.broadcast = function broadcast(data) {
     wss.clients.forEach(function each(client) {
       console.log("client broadcasted to")
         client.send(JSON.stringify(data));
         console.log('Message is being sent to client from server now', data);
-  
-          // Broadcast to everyone else.
     });
   };
-  console.log('Client connected');
+    console.log('Client connected');
     ws.on('message', function incoming(newMessage) {
       const parsedMessage = JSON.parse(newMessage);
       console.log(`User ${parsedMessage.username} said ${parsedMessage.content}`);
-    
-    switch (parsedMessage.message.type) {
-      case 'postMessage': parsedMessage.message.type = 'newMessage';
-      break;
-    case 'postNotification': parsedMessage.message.type = 'newNotification';
+
+// Switch: Post Notification  
+switch (parsedMessage.message.type) {
+  case 'postMessage': parsedMessage.message.type = 'newMessage';
     break;
-    }
-    wss.broadcast(parsedMessage);
-        });
+  case 'postNotification': parsedMessage.message.type = 'newNotification';
+    break;
+}    
+
+//Broadcast
+wss.broadcast(parsedMessage);
+});
 
 wss.clients.forEach(function each(client) {
   client.send(wss.clients.size);
   
 
-  // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-
-  client.on('close', () => {
-    console.log('Client disconnected');
+// Callback: when client closes the socket. This usually means they closed their browser.
+client.on('close', () => {
+  console.log('Client disconnected');
     wss.clients.forEach(function each(client) {
       client.send(wss.clients.size);
     });
   });
-  });
+});
 });
